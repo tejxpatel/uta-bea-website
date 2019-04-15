bd6d787c66748bc5823e6f97d7c7245db89b1721
tejbpatel <tejpatelbusiness@gmail.com>
2019-04-14 19:56:59 -0500

    v3

--- index.php
+++ index.php
@@ -3,13 +3,19 @@
 
 <head>
 
+<script src="https://code.jquery.com/jquery-3.4.0.js" integrity="sha256-DYZMCC8HTC+QDr5QNaIcfR7VSPtcISykd+6eSmBW5qo=" crossorigin="anonymous"></script>
+
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-<link rel="stylesheet" src="css/style.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="js/parallax.js-1.5.0/parallax.js"></script>
+
+
+<link rel="stylesheet" href="css/style.css">
 
 </head>
 
@@ -53,6 +59,7 @@
   </div>
 </nav>
 
+<div class="home-section"> 
 
 <h1>
     Who Are We?
@@ -75,11 +82,26 @@
   </div>
 </div>
 
+</div>
 
+<div class="parallax-container" data-parallax="scroll" data-bleed="10" data-image-src="img/home/placeholder.jpg" data-natural-width="3968" data-natural-height="2645"></div>
+
+
+<div class="home-section"> 
 
 <h1>
     What Do We Do?
-</h1>s
+</h1>
+
+
+
+
+</div>
+
+
+
+<div class="parallax-container" data-parallax="scroll" data-bleed="10" data-image-src="img/home/placeholder.jpg" data-natural-width="3968" data-natural-height="2645"></div>
+
 
 
 
@@ -89,6 +111,9 @@
     <p class="lead">Footer</p>
   </div>
 </div>
+
+
+
 
 
 
