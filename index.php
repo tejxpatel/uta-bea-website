2a55ec5a74b66ee9c320115cd8cea1b0b286289d
Jefferson <jefferson.ondzemangha@mavs.uta.edu>
2019-04-16 11:52:12 -0500

    cleaned up the repo
    should fix all issues

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<meta charset="utf-8">
+<title>BEA UTA Home</title>
+
+<?php include("php-include/head.php"); ?>
+
+</head>
+
+<body>
+
+<div class="container-fluid">
+
+
+
+<!-- ==================== TOP HEADER + NAVIGATION BAR ==================== -->
+
+<?php include("php-include/header.php"); ?>
+
+
+
+
+<!-- ==================== HERO HEADER ==================== -->
+
+
+<div class="parallax-container home-hero" data-parallax="scroll" data-bleed="10" data-image-src="/img/home-page/home-header.jpg">
+
+<h1 class="parallax-text">Welcome to the UTA Chapter of the Broadcast Education Association!</h1>
+
+</div>
+
+
+
+
+<!-- ==================== MAIN CONTENT ==================== -->
+
+<!-- =========== WHO WE ARE SECTION =============== -->
+<div class="home-section">
+<h2 class="center-text">WHO ARE WE</h2>
+
+<br>
+
+
+<div class="container center-text who-we-are">
+
+  <div class="row center-text">
+
+  
+    <div class="col-sm center-text">
+    <i class="fas fa-info-circle"></i>
+      <h3>Students</h3>
+      <p>
+
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+      ut labore et dolore magna aliqua.
+
+      </p>
+    </div>
+    <div class="col-sm center-text">
+
+    <i class="fas fa-info-circle"></i>
+    <h3>Storytellers</h3>
+
+    <p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+ut labore et dolore magna aliqua.
+
+</p>
+    </div>
+    <div class="col-sm center-text">
+    <i class="fas fa-info-circle"></i>
+    <h3>Storytellers</h3>
+
+    <p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+
+</p>
+
+    </div>
+  </div>
+</div>
+
+</div>
+
+
+<!-- =========== PARALLAX SECTION =============== -->
+
+
+<div class="parallax-container" data-parallax="scroll" data-bleed="10" data-image-src="img/temp.jpg"></div>
+
+<!-- =========== WHAT WE DO SECTION =============== -->
+
+<div class="home-section">
+
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h2>WHAT WE DO</h2>
+      <p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+</p>
+
+
+    </div>
+    <div class="col">
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="img/home-page/18422432_217182558797808_3112332205953372463_o.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/home-page/18422432_217182558797808_3112332205953372463_o.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="img/home-page/18422432_217182558797808_3112332205953372463_o.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+
+
+<!-- =========== PARALLAX SECTION =============== -->
+
+
+
+<div class="parallax-container" data-parallax="scroll" data-bleed="10" data-image-src="img/temp.jpg"></div>
+
+
+<!-- ==================== FOOTER ==================== -->
+
+<?php include("php-include/footer.php"); ?>
+
+
+
+
+
+</body>
+
+
+</html>
